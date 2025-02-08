3a2bdd378cc426c3d2f24389d91cfe8650d5ff23
coder-aadii <aditya.aerpule191996@gmail.com>
2025-02-08 18:34:23 +0530

    Set up React frontend and Node.js backend

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('AuraDeck backend is working');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
