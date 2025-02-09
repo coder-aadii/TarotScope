540ae3287aa1bbf864b9d83de898dc7ae9f9f758
coder-aadii <aditya.aerpule191996@gmail.com>
2025-02-09 10:42:19 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,34 +8,16 @@ import TarotReading from './components/TarotReading';
 import Home from './components/Home';
 
 function App() {
-  const [message, setMessage] = useState('');
-
+  // Removed the unused message and setMessage
   useEffect(() => {
     axios.get('http://localhost:5000/')
-      .then(response => {
-        setMessage(response.data);
-      })
       .catch(error => console.error('Error:', error));
   }, []);
 
   return (
     <Router>
       <div className="App">
-        {/* You can uncomment the navigation if needed */}
-        {/* <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/register">Register</Link></li>
-            <li><Link to="/dashboard">Dashboard</Link></li>
-            <li><Link to="/tarot-reading">Tarot Reading</Link></li>
-          </ul>
-        </nav> */}
-
-        {/* <h1>{message}</h1> */}
-
         <Routes>
-          {/* Setting Home as the default route */}
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
